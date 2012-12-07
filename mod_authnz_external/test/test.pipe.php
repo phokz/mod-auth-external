6319e57981296ad9d922c921f59a701569a89057
jan@unixpapa.com <jan@unixpapa.com@8c465660-3f02-11de-a81c-fde7d73ceb89>
2012-12-07 16:41:25 +0000

    Added test/test.pipe.php

---
+++ mod_authnz_external/test/test.pipe.php
@@ -0,0 +1,40 @@
+#!/usr/bin/php
+<?php
+
+// Test authenticator using pipe method.  Logins will be accepted if the
+// login and the password are identical, and will be rejected otherwise.
+//
+// This authenticator does copious logging by writing all sorts of stuff to
+// STDERR.  A production authenticator would not normally do this, and it
+// *especially* would not write the plain text password out to the log file.
+
+// Get the name of this program
+$prog = $argv[0];
+
+// Get the user name
+$user = trim(fgets(STDIN));
+
+// Get the password
+$pass = trim(fgets(STDIN));
+
+// Print them to the error_log file
+fwrite(STDERR, $prog . ": user='" . $user . "' pass='" . $pass . "'\n");
+
+foreach ($_ENV as $k => $v)
+{
+	fwrite(STDERR, $prog . ': ' . $k . '=' . $v . "\n");
+}
+
+// Accept the login if the user name matchs the password
+if ($user == $pass)
+{
+	fwrite(STDERR, $prog . ": login matches password - Accepted\n");
+	exit(0);
+}
+else
+{
+	fwrite(STDERR, $prog . ": login doesn't match password - Rejected\n");
+	exit(1);
+}
+
+?>
